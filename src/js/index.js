8a90fd9a2aaa103dcf559068a4867eb1dbe7406c
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-07-30 13:02:24 +0300

    initiall comit

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+// console.log(window.innerWidth);
+// window.open("https://itproger.com");
+// window.open("https://itproger.com", "Hello", "width=600, height=800");
+
+
+
+// console.log(navigator.language)
+
+// if(navigator.onLine){
+//     console.log("Yes");
+// }
+
+// navigator.geolocation.getCurrentPosition(function(position){
+//     console.log(position.coords.latitude);
+//     console.log(position.coords.longitude);
+// });
+
+// console.log(location.host);
+
