bced3a39df816866f5d28a7e4c2a3c867fa7d483
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-08-01 16:43:32 +0300

    update

--- src/homework/task4/index.js
+++ src/homework/task4/index.js
@@ -1,9 +1,9 @@
 function getAttributes() {
     let link = document.getElementById("link");
 
-    console.log("href:", link.getAttribute("href"));
-    console.log("hreflang:", link.getAttribute("hreflang"));
-    console.log("rel:", link.getAttribute("rel"));
-    console.log("target:", link.getAttribute("target"));
-    console.log("type:", link.getAttribute("type"));
+    console.log("Значення атрибута href:", link.getAttribute("href"));
+    console.log("Значення атрибута hreflang:", link.getAttribute("hreflang"));
+    console.log("Значення атрибута rel:", link.getAttribute("rel"));
+    console.log("Значення атрибута target:", link.getAttribute("target"));
+    console.log("Значення атрибута type:", link.getAttribute("type"));
 }
