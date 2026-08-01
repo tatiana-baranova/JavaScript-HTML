be51ef94b183162c6b6e580e36d1d7019637596b
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-08-01 15:21:11 +0300

    add event listener

--- src/js/events.js
+++ src/js/events.js
@@ -19,3 +19,34 @@ function focusEnd(){
     inputField.style.backgroundColor = 'white';
     inputField.style.padding = '0px';
 }
+
+window.onclick = function(event){
+    let heading = this.document.querySelector('h1.heading');
+
+    if(event.target === heading){
+        heading.style.color = 'red';
+        console.log('Click', event.target);
+    }
+    
+}
+
+let userInput = document.querySelector('#user-input');
+userInput.oninput = function(){
+    console.log(userInput.value);
+}
+
+
+
+// Event Listener//
+
+let block = document.querySelector('div.block');
+
+function handlerBlock(){
+    block.innerHTML = 'You clicked the block!';
+}
+
+block.addEventListener('mouseover', handlerBlock);
+block.addEventListener('mouseleave', function(){
+    block.innerHTML = 'Hello';
+});
+
