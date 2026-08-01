6a7a4bc82bf7dbe221beeecf0f417d03a70f2c07
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-08-01 14:56:05 +0300

    event focus

--- src/js/events.js
+++ src/js/events.js
@@ -7,3 +7,15 @@ function clickText(selector){
     document.querySelector(selector).style.backgroundColor = 'blue';
     document.querySelector(selector).style.color = 'white';
 }
+
+let inputField = document.querySelector('input.input-field');
+
+function focusEvent(){
+    inputField.style.backgroundColor = 'silver';
+    inputField.style.padding = '10px';
+}
+
+function focusEnd(){
+    inputField.style.backgroundColor = 'white';
+    inputField.style.padding = '0px';
+}
