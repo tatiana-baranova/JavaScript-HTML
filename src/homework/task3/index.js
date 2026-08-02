411970e804cf4facedac946be4ec45ac03848d40
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-08-02 17:27:15 +0300

    onkeydown

--- src/homework/task3/index.js
+++ src/homework/task3/index.js
@@ -11,3 +11,16 @@ paragraph.addEventListener('mouseleave', () => {
         word.style.color = 'black';
     });
 });
+
+let textarea = document.querySelector('.full-text');
+textarea.onkeydown = function(event){
+    console.log('onkeydown:', event.key);
+}
+
+textarea.onkeyup = function(event){
+    console.log('onkeyup:', event.key);
+}
+
+textarea.onkeypress = function(event){
+    console.log('onkeypress:', event.key);
+}
