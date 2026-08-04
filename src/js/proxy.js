96127316568d01e75ca18a2e0321d3b576219c7e
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-08-04 15:28:15 +0300

    add proxy

---
+++ src/js/proxy.js
@@ -0,0 +1,34 @@
+const car = {
+    model: "BMW",
+    speed: 230,
+    weight: 1.3
+}
+
+const prox = new Proxy(car, {
+    get(target, prop) {
+        if(prop === "model")
+            console.log("You don't recognise the model");
+        else
+            return target[prop]
+    },
+    set(target, prop, value) {
+        const carProx = ['speed', 'weight', 'color']
+        if(carProx.includes(prop))
+            throw Error('The value cannot be set')
+
+        console.log(`Attempt to set a ${prop} with a ${value}`);
+        if(prop === 'speed' && value < 10){
+            console.log(`It's not possible`);
+            return undefined
+        }
+        target[prop] = value
+    },
+    has(target, prop){
+        if(prop === 'color')
+            return true
+        else if(prop in target)
+            return true
+        else
+            return false
+    }
+})
