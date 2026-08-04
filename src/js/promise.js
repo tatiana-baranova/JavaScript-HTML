7bddde0ab2d3d02037426a2fd1f831249c2f33d7
Tetiana Baranova <syniovych.tanya@gmail.com>
2026-08-04 14:37:10 +0300

    add promise

--- src/js/promise.js
+++ src/js/promise.js
@@ -1,23 +1,51 @@
 // Callback
 
-let jsBetterThanPhp = false;
+let jsBetterThanPhp = true;
 let nodeJSIsGreat = true;
 
 
-function isJsWorthCallback(success, error){
-    if(jsBetterThanPhp && nodeJSIsGreat){
-        success({
-            isWorth: true,
-            name: "JavaScript"
-        })
-    } else
-        error("JavaScript is not worth it");
+// function isJsWorthCallback(success, error){
+//     if(jsBetterThanPhp && nodeJSIsGreat){
+//         success({
+//             isWorth: true,
+//             name: "JavaScript"
+//         })
+//     } else
+//         error("JavaScript is not worth it");
+// }
+
+// isJsWorthCallback((message) => {
+//     console.log("Success: " + message.name);
+
+// }, (error) => {
+//     console.log("Error: " + error);
+    
+// })
+
+function isJsWorthPromise() {
+    return new Promise((resolve, reject) => {
+        if(jsBetterThanPhp && nodeJSIsGreat){
+            resolve({
+                isWorth: true,
+                name: "JavaScript"
+            });
+        } else {
+            reject("JavaScript is not worth it");
+        }
+    });
 }
 
-isJsWorthCallback((message) => {
-    console.log("Success: " + message.name);
-
-}, (error) => {
+isJsWorthPromise()
+    .then((message) => {
+        console.log("Success: " + message.name);
+    })
+    .then(() => {
+        console.log("Message");
+    })
+    .catch((error) => {
     console.log("Error: " + error);
-    
-})
+    })
+    .finally(() => {
+        console.log("All is down");
+        
+    })
